a4cbaef9efd4d8377f31c1562c72dab7a90fc8b4
Maverik <guilherme>
2016-11-18 01:06:13 +0100

    sprk

--- build.sbt
+++ build.sbt
@@ -4,15 +4,17 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "com.typesafe.play" % "play-json_2.11" % "2.5.4"
-libraryDependencies += "org.apache.hadoop" % "hadoop-core" % "1.2.1" % "provided"
-libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.6.4" % "provided"
-libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "1.6.2" % "provided"
-libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "1.6.2" % "provided"
-libraryDependencies += "org.scala-lang" % "scala-library" % "2.11.8"
-
 resolvers ++= Seq("maven.org" at "http://repo2.maven.org/maven2",
   "conjars.org" at "http://conjars.org/repo")
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.2"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.0.2"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.0.2"
+
+//libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.2" % "provided"
+//libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.0.2" % "provided"
+//libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.0.2" % "provided"
+//libraryDependencies += "org.scala-lang" % "scala-library" % "2.11.8"
 
 mainClass in Compile := Some("org.mng.Main")
 
--- src/main/scala/org/mng/FrequentItemset.scala
+++ src/main/scala/org/mng/FrequentItemset.scala
@@ -1,26 +1,45 @@
 package org.mng
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
+case class ItemSet(items: List[Int])
 
 object FrequentItemSet {
 
+  def frequentItemSets(basket: List[Int], frequentItemSets: ItemSet): List[Int] = {
+    List(1, 2)
+  }
+
+  def findFrequentItemSet(baskets: RDD[ItemSet], setSize: Int, frequentItemSet: Option[RDD[ItemSet]]): RDD[ItemSet] ={
+    frequentItemSet.get
+  }
+
   def main(args: Array[String]): Unit = {
 
-    val filePath = args(0)
+    val filePath = "/home/maverik/IO/frequent-itemsets/src/main/resources/T10I4D100K.dat"//args(0)
+    val cores = Runtime.getRuntime.availableProcessors
 
-    val conf = new SparkConf().setAppName("Frequent Itemset")
-    val sc = new SparkContext(conf)
+    val spark = SparkSession
+      .builder()
+      .master(s"local[$cores]")
+      .appName("Frequent Itemset")
+      .getOrCreate()
 
-    val textFile = sc.textFile(filePath)
+    val textFile = spark.read.textFile(filePath)
 
 //    val basketsRDD = textFile.map(_.split(" ").map(_.toInt)).map(list => collection.SortedSet(list: _*))
-    val basketsRDD = textFile.map(_.split(" ").map(_.toInt))
+    val basketsRDD = textFile.map(_.split(" ").toList.map(_.toInt))
     basketsRDD.persist(StorageLevel.MEMORY_AND_DISK)
 
-    basketsRDD.take(5).foreach(_.foreach(x => print(x, " ")))
-
+    basketsRDD.take(5).foreach(
+      values => {
+        values.foreach(x => print(x, " "))
+        print("\n")
+      }
+    )
   }
 
 }
